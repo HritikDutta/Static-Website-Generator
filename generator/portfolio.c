cacf92561f9ae9a78d981639ceac9b37ec646bc3
ThreewayDude <dutta.hritik@gmail.com>
2021-03-19 15:59:02 +0530

    Generator: Implemented portfolio_free() and persona_free().

--- generator/portfolio.c
+++ generator/portfolio.c
@@ -14,9 +14,45 @@ Persona persona_make()
     return (Persona){ 0, 0, abilities, 0, projects };
 }
 
+void persona_free(Persona* persona)
+{
+    if (persona->name)
+        string_free(persona->name);
+
+    if (persona->color)
+        string_free(persona->color);
+
+    if (persona->blerb)
+        string_free(persona->blerb);
+
+    da_foreach(String, ab, persona->abilities)
+        string_free(ab);
+    da_free(persona->abilities);
+
+    da_foreach(String, pj, persona->projects)
+        string_free(pj);
+    da_free(persona->projects);
+}
+
 Portfolio portfolio_make()
 {
     DArray(Persona) personas;
     da_make(personas);
     return (Portfolio){ 0, 0, 0, personas };
+}
+
+void portfolio_free(Portfolio* portfolio)
+{
+    if (portfolio->home_template)
+        string_free(portfolio->home_template);
+
+    if (portfolio->page_template)
+        string_free(portfolio->page_template);
+
+    if (portfolio->outdir)
+        string_free(portfolio->outdir);
+
+    da_foreach(Persona, persona, portfolio->peronas)
+        persona_free(persona);
+    da_free(portfolio->peronas);
 }
